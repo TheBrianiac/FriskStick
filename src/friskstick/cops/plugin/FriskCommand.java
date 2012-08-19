48623cf5e60d2ab5882d41b53b5e6caa49f28f83
domingocool <mrdomingocool@gmail.com>
2012-08-19 13:04:42 -0500

    Put the command in its own class
    Put the frisk command in its own class. Nuff said.

--- src/friskstick/cops/plugin/FriskCommand.java
+++ src/friskstick/cops/plugin/FriskCommand.java
@@ -1,6 +1,7 @@
 package friskstick.cops.plugin;
 
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -12,9 +13,17 @@ import org.bukkit.inventory.PlayerInventory;
  * 
  */
 
-public class FriskCommand extends FriskStick{
+public class FriskCommand implements CommandExecutor{
+	
+	private FriskStick plugin;
+	
+	public FriskCommand(FriskStick plugin) {
+		this.plugin = plugin;
+	}
+	
 	int index = 0;
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 		Player player = (Player)sender;	
 
@@ -32,33 +41,33 @@ public class FriskCommand extends FriskStick{
 
 			} else if(args.length == 1) {
 
-				Player frisked = this.getServer().getPlayer(args[0]);
+				Player frisked = plugin.getServer().getPlayer(args[0]);
 
 				if(player.hasPermission("friskstick.chat")) {
 
 					PlayerInventory inventory = frisked.getInventory();
 					boolean found = false;
 
-					for(String drug: getConfig().getStringList("drug-ids")) {
+					for(String drug: plugin.getConfig().getStringList("drug-ids")) {
 
 						if(drug.contains(":")) {
 
 							String firsthalf = drug.split(":")[0];
 							String lasthalf = drug.split(":")[1];
 
-							for(int i = 1; i <= getConfig().getInt("amount-to-search-for"); i++) {
+							for(int i = 1; i <= plugin.getConfig().getInt("amount-to-search-for"); i++) {
 
 								ItemStack[] contents = inventory.getContents();
 								if(inventory.contains(new ItemStack(Integer.parseInt(firsthalf), i, Short.parseShort(lasthalf)))) {
 
 									player.getInventory().addItem(new ItemStack(contents[inventory.first(new ItemStack(Integer.parseInt(firsthalf), i, Short.parseShort(lasthalf)))]));
 									inventory.removeItem(new ItemStack(Integer.parseInt(firsthalf), 2305, Short.parseShort(lasthalf)));
-									player.sendMessage(getConfig().getString("cop-found-msg").replaceAll("&", "§").replaceAll("%itemname%", getConfig().getStringList("drug-names").toArray()[index].toString()).replaceAll("%player%", this.getServer().getPlayer(args[0]).getName()));
-									this.getServer().getPlayer(args[0]).sendMessage(getConfig().getString("player-found-msg").replaceAll("&", "§").replaceAll("%player%", player.getName()).replaceAll("%itemname%", getConfig().getStringList("drug-names").toArray()[index].toString()));
+									player.sendMessage(plugin.getConfig().getString("cop-found-msg").replaceAll("&", "§").replaceAll("%itemname%", plugin.getConfig().getStringList("drug-names").toArray()[index].toString()).replaceAll("%player%", plugin.getServer().getPlayer(args[0]).getName()));
+									plugin.getServer().getPlayer(args[0]).sendMessage(plugin.getConfig().getString("player-found-msg").replaceAll("&", "§").replaceAll("%player%", player.getName()).replaceAll("%itemname%", plugin.getConfig().getStringList("drug-names").toArray()[index].toString()));
 
 									if(player.hasPermission("friskstick.jail")) {
 
-										jail(this.getServer().getPlayer(args[0]).getName());
+										jail(plugin.getServer().getPlayer(args[0]).getName());
 
 									}
 
@@ -76,12 +85,12 @@ public class FriskCommand extends FriskStick{
 								ItemStack[] contents = inventory.getContents();
 								player.getInventory().addItem(new ItemStack(contents[inventory.first(drugid)]));
 								inventory.removeItem(new ItemStack(drugid, 2305));
-								player.sendMessage(getConfig().getString("cop-found-msg").replaceAll("&", "§").replaceAll("%itemname%", getConfig().getStringList("drug-names").toArray()[index].toString()).replaceAll("%player%", this.getServer().getPlayer(args[0]).getName()));
-								this.getServer().getPlayer(args[0]).sendMessage(getConfig().getString("player-found-msg").replaceAll("&", "§").replaceAll("%player%", player.getName()).replaceAll("%itemname%", getConfig().getStringList("drug-names").toArray()[index].toString()));
+								player.sendMessage(plugin.getConfig().getString("cop-found-msg").replaceAll("&", "§").replaceAll("%itemname%", plugin.getConfig().getStringList("drug-names").toArray()[index].toString()).replaceAll("%player%", plugin.getServer().getPlayer(args[0]).getName()));
+								plugin.getServer().getPlayer(args[0]).sendMessage(plugin.getConfig().getString("player-found-msg").replaceAll("&", "§").replaceAll("%player%", player.getName()).replaceAll("%itemname%", plugin.getConfig().getStringList("drug-names").toArray()[index].toString()));
 
 								if(player.hasPermission("friskstick.jail")) {
 
-									jail(this.getServer().getPlayer(args[0]).getName());
+									jail(plugin.getServer().getPlayer(args[0]).getName());
 
 								}
 
@@ -99,8 +108,8 @@ public class FriskCommand extends FriskStick{
 
 					if(!found) {
 
-						player.sendMessage(getConfig().getString("cop-not-found-msg").replaceAll("&", "§").replaceAll("%player%", this.getServer().getPlayer(args[0]).getName()));
-						this.getServer().getPlayer(args[0]).sendMessage(getConfig().getString("player-not-found-msg").replaceAll("&", "§").replaceAll("%player%", player.getName()));
+						player.sendMessage(plugin.getConfig().getString("cop-not-found-msg").replaceAll("&", "§").replaceAll("%player%", plugin.getServer().getPlayer(args[0]).getName()));
+						plugin.getServer().getPlayer(args[0]).sendMessage(plugin.getConfig().getString("player-not-found-msg").replaceAll("&", "§").replaceAll("%player%", player.getName()));
 
 						if(player.getHealth() >= 2) {
 
@@ -130,17 +139,17 @@ public class FriskCommand extends FriskStick{
 
 	public void jail(String name) {
 
-		if(this.getConfig().getBoolean("auto-jail")) {
+		if(plugin.getConfig().getBoolean("auto-jail")) {
 
-			if(this.getServer().getPluginManager().isPluginEnabled("Essentials")) {
+			if(plugin.getServer().getPluginManager().isPluginEnabled("Essentials")) {
 
-				if(getConfig().getInt("time-in-jail") > 0) {
+				if(plugin.getConfig().getInt("time-in-jail") > 0) {
 
-					this.getServer().dispatchCommand(this.getServer().getConsoleSender(), "jail " + name + " " + this.getConfig().getString("jail-name") + " " + (this.getConfig().getInt("time-in-jail") + 1));
+					plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), "jail " + name + " " + plugin.getConfig().getString("jail-name") + " " + (plugin.getConfig().getInt("time-in-jail") + 1));
 
-				} else if(getConfig().getInt("time-in-jail") == -1) {
+				} else if(plugin.getConfig().getInt("time-in-jail") == -1) {
 
-					this.getServer().dispatchCommand(this.getServer().getConsoleSender(), "jail " + name + " " + this.getConfig().getString("jail-name"));
+					plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), "jail " + name + " " + plugin.getConfig().getString("jail-name"));
 
 				}
 
