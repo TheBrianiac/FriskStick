38629d2e6532b59af0b6a9ca8870fb8934472afb
domingocool <mrdomingocool@gmail.com>
2012-08-21 19:39:35 -0500

    Class to Check for Updates
    Just helps the user so he/she doesn't have to always go to the page to
    check for an update.

---
+++ src/friskstick/cops/plugin/PluginUpdateCheck.java
@@ -0,0 +1,11 @@
+package friskstick.cops.plugin;
+
+public class PluginUpdateCheck {
+	
+	/*
+	 * 
+	 * Check for new versions of FriskStick and if the player is an OP, send message on Login that he/she should update the plugin.
+	 * 
+	 */
+	
+}
