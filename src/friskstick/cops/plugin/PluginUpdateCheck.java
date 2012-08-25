c4cb3e8f19246e8d0fdbacae461f94a157292f9b
domingocool <mrdomingocool@gmail.com>
2012-08-25 10:06:07 -0500

    Spacing and such

--- src/friskstick/cops/plugin/PluginUpdateCheck.java
+++ src/friskstick/cops/plugin/PluginUpdateCheck.java
@@ -13,84 +13,147 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public class PluginUpdateCheck implements Listener{
+public class PluginUpdateCheck implements Listener {
+	
 	private int recentVersion = 0;
 	private int currentVersion = 0;
 	private String files = "http://dev.bukkit.org/server-mods/friskstick/";
 	private PluginDescriptionFile pdf;
 
-	public PluginUpdateCheck(FriskStick plugin){
+	public PluginUpdateCheck(FriskStick plugin) {
+		
 		pdf = plugin.getDescription();
+		
 	}
 
-	public String getCurrentVersion(){
+	public String getCurrentVersion() {
+		
 		Logger logger = Logger.getLogger("Minecraft");
-		try{
+		
+		try {
+			
 			URL filepage = new URL(files);
 			BufferedReader reader = new BufferedReader(new InputStreamReader(filepage.openStream()));
 			String versions;
 			String currentVersion = "";
-			while((versions = reader.readLine()) != null){
-				if(versions.trim().equalsIgnoreCase("<dt>Recent files</dt>")){
+			
+			while((versions = reader.readLine()) != null) {
+				
+				if(versions.trim().equalsIgnoreCase("<dt>Recent files</dt>")) {
+					
 					currentVersion = removeTags(reader.readLine());
+					
 				}
+				
 			}
+			
 			reader.close();
-			if(currentVersion.equalsIgnoreCase("")){
+			
+			if(currentVersion.equalsIgnoreCase("")) {
+				
 				logger.log(Level.WARNING, "Could not check for updates!");
-			}else{
+				
+			} else {
+				
 				String[] fileVersions = currentVersion.split("A: ");
 				return fileVersions[1].trim();
+				
 			}
-		}catch(Exception e){
+			
+		} catch(Exception e) {
+			
 			logger.log(Level.WARNING, "Could not check for updates!");
+			
 		}
+		
 		return null;
+		
 	}
-	private String removeTags(String HTMLcode){
+	
+	private String removeTags(String HTMLcode) {
+		
 		String removedTags = "";
 		boolean isTag = false;
-		for(int i = 0; i < HTMLcode.length(); i++){
-			if(HTMLcode.charAt(i) == '<'){
+		
+		for(int i = 0; i < HTMLcode.length(); i++) {
+			
+			if(HTMLcode.charAt(i) == '<') {
+				
 				isTag = true;
-			}else if(HTMLcode.charAt(i) == '>'){
-				if(!isTag){
+				
+			} else if(HTMLcode.charAt(i) == '>') {
+				
+				if(!isTag) {
+					
 					removedTags += HTMLcode.charAt(i);
-				}else{
+					
+				} else {
+					
 					isTag = false;
+					
 				}
-			}else{
-				if(!isTag){
+				
+			} else {
+				
+				if(!isTag) {
+					
 					removedTags += HTMLcode.charAt(i);
+					
 				}
+				
 			}
+			
 		}
+		
 		return removedTags;
+		
 	}
-	public void checkForUpdates(){
+	
+	public void checkForUpdates() {
+		
 		String recent = this.getCurrentVersion().split(".")[2].trim();
 		String current = pdf.getVersion().split("Alpha")[0].trim();
 		StringBuilder rc = new StringBuilder();
 		StringBuilder cc = new StringBuilder();
-		for(int i = 0; i < recent.split(".").length; i++){
+		
+		for(int i = 0; i < recent.split(".").length; i++) {
+			
 			rc.append(recent.split(".")[i]);
+			
 		}
-		for(int i = 0; i < current.split(".").length; i++){
+		
+		for(int i = 0; i < current.split(".").length; i++) {
+			
 			cc.append(current.split(".")[i]);
+			
 		}
+		
 		recentVersion = Integer.parseInt(rc.toString());
 		currentVersion = Integer.parseInt(cc.toString());
+		
 	}
+	
 	@EventHandler
-	public void Check(PlayerJoinEvent event){
+	public void checkJoin(PlayerJoinEvent event) {
+		
 		this.checkForUpdates();
-		if(currentVersion != 0 || recentVersion != 0){
-			if(currentVersion < recentVersion){
+		
+		if(currentVersion != 0 || recentVersion != 0) {
+			
+			if(currentVersion < recentVersion) {
+				
 				Player player = event.getPlayer();
-				if(player.hasPermission("friskstick.notify")){
+				
+				if(player.hasPermission("friskstick.notify")) {
+					
 					player.sendMessage(ChatColor.DARK_AQUA + "FriskStick has been updated to: " + this.getCurrentVersion());
+					
 				}
+				
 			}
+			
 		}
+		
 	}
+	
 }
