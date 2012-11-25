fffa692b1580bea6c1dc2769b18adfcf4ceaf42e
Domingo Mesa <mrdomingocool@gmail.com>
2012-11-25 15:02:36 -0600

    Added some stuff to the FriskStick command
    Made it so that it does something.

--- src/friskstick/cops/commands/FriskStickHelpCommand.java
+++ src/friskstick/cops/commands/FriskStickHelpCommand.java
@@ -1,23 +1,52 @@
 package friskstick.cops.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import friskstick.cops.plugin.FriskStick;
 
 public class FriskStickHelpCommand implements CommandExecutor {
 
-	private FriskStick plugin;
-
 	public FriskStickHelpCommand(FriskStick plugin) {
-
-		this.plugin = plugin;
 
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+
+		Player player = null;
+
+		if(sender instanceof Player) {
+
+			player = (Player)sender;
+
+		}
+
+		if(player == null) {
+
+			sender.sendMessage("You cannot run this command in the console!");
+
+		} else {
+
+			if(commandLabel.equalsIgnoreCase("friskstick")) {
+
+				if(player.hasPermission("friskstick.help") || player.isOp()) {
+
+					player.sendMessage(ChatColor.AQUA + "***************FriskStick Help***************");
+					player.sendMessage(ChatColor.DARK_AQUA + "This plugin frisks players for illegal items.");
+
+				} else {
+
+					player.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
+
+				}
+
+			}
+
+		}
 
 		return false;
 	}
